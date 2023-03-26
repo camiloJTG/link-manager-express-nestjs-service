fc17924f7bebef4dfd46352ebf0e8e1c1364ca65
camiloJTG <camilojtg.devep@protonmail.com>
2023-03-25 21:34:06 -0300

    docs: Implement documentation in endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { swaggerDoc } from '../common/decorators/swagger-doc.decorator';
 import { AccessLoginDto } from './dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @swaggerDoc('auth')
   @Post('login')
   login(@Body() accessLoginDto: AccessLoginDto) {
     return this.authService.login(accessLoginDto);
